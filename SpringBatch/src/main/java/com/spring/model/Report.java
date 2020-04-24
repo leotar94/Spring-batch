da7df6fe3d6fd8b668bd2d6eaae3658eae5fe6f8
shini <shini@DESKTOP-UK5S079>
2020-04-24 13:08:20 +0200

    third

---
+++ SpringBatch/src/main/java/com/spring/model/Report.java
@@ -0,0 +1,34 @@
+package com.spring.model;
+
+public class Report {
+	private String Date;
+	private String Impressions;
+	private String Clicks;
+	private String Earnings;
+	
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String date) {
+		Date = date;
+	}
+	public String getImpressions() {
+		return Impressions;
+	}
+	public void setImpressions(String impressions) {
+		Impressions = impressions;
+	}
+	public String getClicks() {
+		return Clicks;
+	}
+	public void setClicks(String clicks) {
+		Clicks = clicks;
+	}
+	public String getEarnings() {
+		return Earnings;
+	}
+	public void setEarnings(String earnings) {
+		Earnings = earnings;
+	}
+	
+}
